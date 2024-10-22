61cbec4f7a33a6efce91775007365b2b0614e5da
vedsu <90701239+vedsu@users.noreply.github.com>
2024-10-22 16:27:35 +0530

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,7 @@ def fixture():
         match_data = Utility.get_fixture()
         return jsonify(match_data),200
 
-@app.route('/<team>', methods =['GET'])
+@app.route('/team/<regno>', methods =['GET'])
 def team_detail(team):
     if request.method == 'GET':
         player_data,team_data = Utility.get_details_by_team(team)
